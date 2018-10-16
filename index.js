8180d4884c9d7ca5324897c25413b4d1a8a23ea7
Chris Cardle <cdcardle@gmail.com>
2018-10-16 15:23:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
   this.veto = () => "No, I must disagree";
+  this.approve = () => "You can do that!";
+  this.doCharity = () => "I like to help people.";
+  this.releasePressStatement = () => "You will see great things from Scuber.";
+  this.sayHi = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
